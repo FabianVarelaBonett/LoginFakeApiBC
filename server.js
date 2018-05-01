2d63c5d4301e3b155dffa034e38effc57357eefb
Fabián Varela Bonett <jfabian.303@gmail.com>
2018-05-01 14:19:07 -0500

    Add CORS Origin

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var port = process.env.PORT || 8081;
 var router = express.Router();
